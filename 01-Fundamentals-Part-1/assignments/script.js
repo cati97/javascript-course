fb421c87e794272cc450b323e4c346c2c62c565b
unknown <katasiw97@gmail.com>
2023-12-11 16:50:34 +0100

    notes on functions, create spread operator args function

--- 01-Fundamentals-Part-1/assignments/script.js
+++ 01-Fundamentals-Part-1/assignments/script.js
@@ -1,4 +1,17 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!!!");
+// if (js === "amazing") alert("JavaScript is FUN!!!");
 js = "boring";
 console.log(20 + 5);
+
+const calcAvg = (a, b, c) => (a + b + c) / 3;
+
+const calcAvgSpread = (...args) => args.reduce((acc, curr) => acc + curr, 0) / args.length;
+
+function calculateAverage() {
+  console.log(arguments);
+  return [...arguments].reduce((acc, curr) => acc + curr, 0) / arguments.length;
+}
+
+console.log(calcAvg(5, 4, 3));
+console.log("calcAvgSpread", calcAvgSpread(5, 4, 3));
+console.log("calculateAverage", calculateAverage(5, 4, 3));
