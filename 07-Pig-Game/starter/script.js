b1bd114769c4b0ab4c5b0460274146fa55669979
unknown <katasiw97@gmail.com>
2024-01-24 17:33:57 +0100

    implement reset game at start of pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+
+const diceEl = document.querySelector('.dice');
+
+const resetGame = () => {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+};
+
+resetGame();
