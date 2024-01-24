ad8997f81592e91a53f17d1dfd2f4c94eb837e28
unknown <katasiw97@gmail.com>
2024-01-24 14:39:40 +0100

    implement modal with overlay

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const showModalButtons = document.querySelectorAll('.show-modal');
+
+const modal = document.querySelector('.modal'); // change style to display: block on event click on any of showModalButtons
+const closeBtn = document.querySelector('.close-modal'); // add event handler to change style to display hidden again on click
+const overlay = document.querySelector('.overlay'); // add event handler to change style to display hidden again on click
+
+const handleOpenModal = () => {
+  modal.style.display = 'block';
+  overlay.style.display = 'block';
+};
+
+const handleCloseModal = () => {
+  modal.style.display = 'none';
+  overlay.style.display = 'none';
+};
+
+showModalButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    handleOpenModal();
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  handleCloseModal();
+});
+
+overlay.addEventListener('click', () => {
+  handleCloseModal();
+});
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    handleCloseModal();
+  }
+});
