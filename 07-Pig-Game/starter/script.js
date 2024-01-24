bbbb43c87b2c331e0b0e2f1f07ed90f2b7fafb2b
unknown <katasiw97@gmail.com>
2024-01-24 18:18:20 +0100

    implement hold score feature

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -5,17 +5,14 @@
 let currentScorePlayer0 = 0;
 let currentScorePlayer1 = 0;
 
-let mainScorePlayer0 = 0;
-let mainScorePlayer1 = 0;
+let totalScorePlayer0 = 0;
+let totalScorePlayer1 = 0;
 
 let activePlayer = 'player--0';
 
 // Selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
-
-const diceEl = document.querySelector('.dice');
-const rollDiceBtn = document.querySelector('.btn--roll');
+const totalScore0El = document.querySelector('#score--0');
+const totalScore1El = document.querySelector('#score--1');
 
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
@@ -23,11 +20,31 @@ const player1El = document.querySelector('.player--1');
 const currentScore0El = document.getElementById('current--0');
 const currentScore1El = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
+const rollDiceBtn = document.querySelector('.btn--roll');
+const holdScoreBtn = document.querySelector('.btn--hold');
+
 // Common functions
 const resetGame = () => {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
+  totalScore0El.textContent = totalScorePlayer0;
+  totalScore1El.textContent = totalScorePlayer1;
   diceEl.classList.add('hidden');
+};
+
+const switchPlayer = () => {
+  if (activePlayer === 'player--0') {
+    currentScorePlayer0 = 0;
+    currentScore0El.textContent = currentScorePlayer0;
+    activePlayer = 'player--1';
+    player1El.classList.add('player--active');
+    player0El.classList.remove('player--active');
+  } else {
+    currentScorePlayer1 = 0;
+    currentScore1El.textContent = currentScorePlayer1;
+    activePlayer = 'player--0';
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+  }
 };
 
 const rollDice = () => {
@@ -35,16 +52,7 @@ const rollDice = () => {
   diceEl.src = `dice-${randomNumber}.png`;
   diceEl.classList.remove('hidden');
   if (randomNumber === 1) {
-    // switch player
-    if (activePlayer === 'player--0') {
-      activePlayer = 'player--1';
-      player1El.classList.add('player--active');
-      player0El.classList.remove('player--active');
-    } else {
-      activePlayer = 'player--0';
-      player0El.classList.add('player--active');
-      player1El.classList.remove('player--active');
-    }
+    switchPlayer();
   } else {
     // add points to current score
     if (activePlayer === 'player--0') {
@@ -57,8 +65,20 @@ const rollDice = () => {
   }
 };
 
+const holdScore = () => {
+  if (activePlayer === 'player--0') {
+    totalScorePlayer0 += currentScorePlayer0;
+    totalScore0El.textContent = totalScorePlayer0;
+  } else {
+    totalScorePlayer1 += currentScorePlayer1;
+    totalScore1El.textContent = totalScorePlayer1;
+  }
+  switchPlayer();
+};
+
 // Reset game at page reload
 resetGame();
 
 // Event listeners
 rollDiceBtn.addEventListener('click', rollDice);
+holdScoreBtn.addEventListener('click', holdScore);
