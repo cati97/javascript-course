395e2ab45b0d3e5a0dce0f528a4015cb50d80cb4
unknown <katasiw97@gmail.com>
2024-01-24 19:31:21 +0100

    improve the code after watching videos of solution

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,12 +1,6 @@
 'use strict';
 
-let currentScorePlayer0 = 0;
-let currentScorePlayer1 = 0;
-
-let totalScorePlayer0 = 0;
-let totalScorePlayer1 = 0;
-
-let activePlayer = 'player--0';
+let totalScores, currentScore, activePlayer, playing;
 
 const totalScore0El = document.querySelector('#score--0');
 const totalScore1El = document.querySelector('#score--1');
@@ -22,65 +16,93 @@ const rollDiceBtn = document.querySelector('.btn--roll');
 const holdScoreBtn = document.querySelector('.btn--hold');
 const newGameBtn = document.querySelector('.btn--new');
 
-const resetGame = () => {
-  currentScorePlayer0 = 0;
-  currentScorePlayer1 = 0;
-  totalScorePlayer0 = 0;
-  totalScorePlayer1 = 0;
+const initGame = () => {
+  totalScores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-  totalScore0El.textContent = totalScorePlayer0;
-  totalScore1El.textContent = totalScorePlayer1;
-  currentScore0El.textContent = currentScorePlayer0;
-  currentScore1El.textContent = currentScorePlayer1;
+  totalScore0El.textContent = 0;
+  totalScore1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
 
   diceEl.classList.add('hidden');
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
 };
 
 const switchPlayer = () => {
-  if (activePlayer === 'player--0') {
-    currentScorePlayer0 = 0;
-    currentScore0El.textContent = currentScorePlayer0;
-    activePlayer = 'player--1';
-    player1El.classList.add('player--active');
-    player0El.classList.remove('player--active');
-  } else {
-    currentScorePlayer1 = 0;
-    currentScore1El.textContent = currentScorePlayer1;
-    activePlayer = 'player--0';
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
-  }
+  // 1. Reset currentScore
+  currentScore = 0;
+  // 2. Display new reset current score on active player
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  // 3. Switch the active player
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // 4. Toggle player--active class if it wasn't there - add it, if it was - remove it
+  player1El.classList.toggle('player--active');
+  player0El.classList.toggle('player--active');
 };
 
 const rollDice = () => {
-  const randomNumber = Math.floor(Math.random() * 6) + 1;
-  diceEl.src = `dice-${randomNumber}.png`;
-  diceEl.classList.remove('hidden');
-  if (randomNumber === 1) {
-    switchPlayer();
-  } else {
-    if (activePlayer === 'player--0') {
-      currentScorePlayer0 += randomNumber;
-      currentScore0El.textContent = currentScorePlayer0;
+  if (playing) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    diceEl.src = `dice-${randomNumber}.png`;
+    diceEl.classList.remove('hidden');
+    if (randomNumber === 1) {
+      switchPlayer();
     } else {
-      currentScorePlayer1 += randomNumber;
-      currentScore1El.textContent = currentScorePlayer1;
+      // 1. Add random number to current score
+      currentScore += randomNumber;
+      // 2. Display new current number in active player
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     }
   }
 };
 
 const holdScore = () => {
-  if (activePlayer === 'player--0') {
-    totalScorePlayer0 += currentScorePlayer0;
-    totalScore0El.textContent = totalScorePlayer0;
-  } else {
-    totalScorePlayer1 += currentScorePlayer1;
-    totalScore1El.textContent = totalScorePlayer1;
+  if (playing) {
+    // 1. Add currentScore to totalScore of the activePlayer
+    totalScores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      totalScores[activePlayer];
+
+    // 2. If totalScore more >= 100 finish game
+    if (totalScores[activePlayer] >= 20) {
+      // Finish the game
+
+      // a. Reset the current score
+      currentScore = 0;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+
+      // b. Add player-winner class to the active player to change styling, remove player active class
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+
+      // c. Set playing to false
+      playing = false;
+
+      // d. Hide the dice
+      diceEl.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
   }
-  switchPlayer();
 };
 
-resetGame();
+initGame();
 rollDiceBtn.addEventListener('click', rollDice);
 holdScoreBtn.addEventListener('click', holdScore);
-newGameBtn.addEventListener('click', resetGame);
+newGameBtn.addEventListener('click', initGame);
