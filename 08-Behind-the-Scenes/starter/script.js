796f872f78ec67488baa6a1b9ee3683cfb8178a1
unknown <katasiw97@gmail.com>
2024-02-07 20:20:41 +0100

    this keyword scope, nested functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+function getFancyName(name) {
+  console.log(this);
+  return name;
+}
+
+console.log(this);
+getFancyName('Fancy');
+
+function outerFunction() {
+  const getFancyNameArrow = name => {
+    console.log(this);
+    return name;
+  };
+  getFancyNameArrow('fancy');
+}
+
+outerFunction();
+
+const person = {
+  age: 25,
+  getAge: () => {
+    return this.age;
+  },
+};
+
+console.log(person.getAge());
+
+function printFirstName() {
+  console.log(firstName);
+
+  function printLastName() {
+    if (firstName === 'Anna') {
+      var lastName = 'Smith';
+    }
+    console.log(lastName);
+  }
+
+  printLastName();
+}
+
+const firstName = 'Anna';
+printFirstName();
