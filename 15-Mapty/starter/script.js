5864f888b42acfb761677a3f2c01b3e8518aeaf8
unknown <katasiw97@gmail.com>
2024-03-25 19:01:49 +0100

    refactor code to App class, using binding for correct this

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,56 +11,78 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class App {
+  workouts = [];
+  #map;
+  #mapEvent;
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField); // in _toggleElevationField we don't use the this keyword so no need for binding
+  }
+
+  // protected method - just a convention - now we have real private class methods using #
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this), // if we just pass this._loadMap - this (class obj) won't be passed down - so in loadMap this would be undefined
+      // this._loadMap - would just call it like a regular function - in regular functions this is undefined!
+      function () {
+        alert('Could not get your position!');
+        ``;
+      }
+    );
+  }
+
+  _loadMap(position) {
     const { latitude, longitude } = position.coords;
     const coords = [latitude, longitude];
     console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
     // I can use L directly because it is a global variable in the script loaded BEFORE this one
-    map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
-    map.on('click', function (mapE) {
-      mapEvent = mapE; // to get global access to it
-      inputDistance.focus();
-      form.classList.remove('hidden');
-    });
-  },
-  function () {
-    alert('Could not get your position!');
+    this.#map.on('click', this._showForm.bind(this));
   }
-);
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault(); // prevents reloading the page on submit
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    inputDistance.focus();
+    form.classList.remove('hidden');
+  }
 
-  // clear all inputs
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup('Workout', {
-      maxWidth: 250,
-      minWidth: 100,
-      autoClose: false,
-      closeOnClick: false,
-      className: 'running-popup',
-    })
-    .openPopup();
-});
+  _newWorkout(e) {
+    console.log(this); // without binding it would point to form HTML element - element to which addEventListener is attached to
+    e.preventDefault(); // prevents reloading the page on submit
 
-inputType.addEventListener('change', function () {
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-});
+    // clear all inputs
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup('Workout', {
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+      .openPopup();
+  }
+}
+
+const app = new App();
