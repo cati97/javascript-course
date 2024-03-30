1c4f9316c61ecd2783d188dc190f092edf27c094
unknown <katasiw97@gmail.com>
2024-03-30 17:24:32 +0100

    create two async requests running one after the other using XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,52 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const renderCountry = (country, className) => {
+  const countryEl = `
+    <article class="country ${className}">
+        <img class="country__img" alt="${country.flags.alt}" src="${
+    country.flags.png
+  }" />
+        <div class="country__data">
+            <h3 class="country__name">${country.name.common}</h3>
+            <h4 class="country__region">${country.region}</h4>
+            <p class="country__row"><span>👫</span>${new Intl.NumberFormat(
+              navigator.language
+            ).format(country.population)}</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(country.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.keys(country.currencies)[0]
+            }</p>
+        </div>
+    </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', countryEl);
+  countriesContainer.style.opacity = 1;
+};
+
+const getAndRenderCountryByName = name => {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${name}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [country] = JSON.parse(this.responseText);
+    renderCountry(country);
+
+    const neighbors = country.borders;
+    const req = new XMLHttpRequest();
+    req.open('GET', `https://restcountries.com/v3.1/alpha/${neighbors[0]}`);
+    req.send();
+
+    req.addEventListener('load', function () {
+      const [country] = JSON.parse(this.responseText);
+      if (!country) return;
+      renderCountry(country, 'neighbour');
+    });
+  });
+};
+
+getAndRenderCountryByName('portugal');
